42e6c7f128de4bf4b27e57a51bb7a6d6c35efb2b
Mark Gillespie <marknh39@hotmail.com>
2026-03-12 12:42:34 -0700

    added material to the sharedcontent, some app changes

---
+++ MakeJsonFile/ImportMaterialContent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeJsonFile
+{
+    internal class ImportMaterialContent
+    {
+        public required string Name { get; set; }
+        public required string ScientificName { get; set; }
+
+        internal static bool MaterialNeedsUpdating(string inpath, DateOnly lastUpdated)
+        {
+            try
+            {
+                DateOnly fileLastUpdated = DateOnly.FromDateTime(System.IO.File.GetLastWriteTime(inpath));
+                return fileLastUpdated > lastUpdated;
+            }
+            catch (FileNotFoundException ex)
+            {
+                return true;
+            }
+        }
+        internal static List<ImportMaterialContent> ImportContents(string inpath)
+        {
+            List<ImportMaterialContent> list = new List<ImportMaterialContent>();
+            using var reader = new StreamReader(inpath);
+            reader.ReadLine(); // skip header
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (!string.IsNullOrEmpty(line))
+                {
+                    var values = ImportColorContent.csvRegex.Split(line); // Assuming simple CSV without quoted fields
+                    list.Add(new ImportMaterialContent { Name = values[0], ScientificName = values[1] });
+                }
+            }
+            return list;
+        }
+    }
+}
--- MakeJsonFile/Program.cs
+++ MakeJsonFile/Program.cs
@@ -1,32 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MakeJsonFile;
+using System.Reflection;
 
 // TODO: update the manifest, to signal to the filament app that there are changes to the content, so that it will reimport the json file
-var outpath = Path.Combine("C:\\Users\\markn\\source\\repos\\MakeJsonFile\\MakeJsonFile", "SharedFilamentData", "Colors Master.json");
-var manifestPath = Path.Combine("C:\\Users\\markn\\source\\repos\\MakeJsonFile\\MakeJsonFile", "SharedFilamentData", "Manifest.json");
-var inpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Colors Master.csv");
-
+var executingPath = Assembly.GetExecutingAssembly().Location;
+var projectDirectory = executingPath.Substring(0, executingPath.IndexOf("\\bin"));  //this is a bit hacky but it works, we want to get the path to the project directory, which is the parent of the bin directory where the executable is located
+var colorsOutpath = Path.Combine(projectDirectory, "SharedFilamentData", "Colors Master.json");
+var manifestPath = Path.Combine(projectDirectory, "SharedFilamentData", "Manifest.json");
+var materialOutpath = Path.Combine(projectDirectory, "SharedFilamentData", "material.json");
+var colorsInpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Colors Master.csv");
+var materialInpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Material Master.csv");
+var inpathVendors = Path.Combine(projectDirectory, "SharedFilamentData", "Vendors.json");
 var manifest = Manifest.GetManifest(manifestPath);
 var colorsUpdated = false;
 var webManifest = await WebManifest.GetWebManifestAsync();
-if (!CheckIfContentsNeedsUpdating(inpath, manifest.ColorsLastUpdated))
-{
-    Console.WriteLine("Color Masters are up to date, no need to update the json file.");
-    return;
-}
-else
+bool updateVendors = false;
+bool materialUpdated = false;
+if (CheckIfContentsNeedsUpdating(colorsInpath, manifest.ColorsLastUpdated))
 {
     Console.WriteLine("Contents need updating, updating the json file.");
-    var json = System.Text.Json.JsonSerializer.Serialize(ImportColorContent.ImportContents(inpath), new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
-    File.WriteAllText(outpath, json);
+    var json = System.Text.Json.JsonSerializer.Serialize(ImportColorContent.ImportContents(colorsInpath), new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+    File.WriteAllText(colorsOutpath, json);
     colorsUpdated = true;
 }
+if (CheckIfVendorsNeedsUpdating(inpathVendors, webManifest.VendorsLastUpdated))
+    updateVendors = true;
+if (CheckIfContentsNeedsUpdating(materialInpath, webManifest.MaterialLastUpdated))
+{
+    Console.WriteLine("Material needs updating, updating the material json file.");
+    var materials = ImportMaterialContent.ImportContents(materialInpath);
+    var json = System.Text.Json.JsonSerializer.Serialize(materials, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+    File.WriteAllText(materialOutpath, json);
+    materialUpdated = true;
+}
 
-if (colorsUpdated)
-    Manifest.UpdateManifest(Path.Combine("C:\\Users\\markn\\source\\repos\\MakeJsonFile\\MakeJsonFile", "SharedFilamentData", "Manifest.json"), colorUpdated: colorsUpdated, vendorsUpdated: false, materialUpdated: false);
+if (colorsUpdated || updateVendors || materialUpdated)
+    Manifest.UpdateManifest(Path.Combine(projectDirectory, "SharedFilamentData", "Manifest.json"), colorUpdated: colorsUpdated, vendorsUpdated: updateVendors, materialUpdated: materialUpdated);
 
 static bool CheckIfContentsNeedsUpdating(string path, DateOnly lastUpdated)
 {
     DateOnly fileLastUpdated = DateOnly.FromDateTime(System.IO.File.GetLastWriteTime(path));
+    return fileLastUpdated > lastUpdated;
+}
+
+static bool CheckIfVendorsNeedsUpdating(string inpath, DateOnly lastUpdated)
+{
+    DateOnly fileLastUpdated = DateOnly.FromDateTime(System.IO.File.GetLastWriteTime(inpath));
+    //result = fileLastUpdated > lastUpdated;
     return fileLastUpdated > lastUpdated;
 }
